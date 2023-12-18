d385063cf68ab44ad23c52ffba177d7da9904bd7
Vikram Kumar <vikrambr8084@gmail.com>
2023-12-18 23:54:46 +0530

    diff data type

---
+++ datatyp.py
@@ -0,0 +1,9 @@
+#here we can use different data type
+a=complex(5,7)
+b=4
+c=4.44
+d="vikram"
+print("The data type of a is ",type(a))
+print("The data type of b is ",type(b))
+print("The data type of c is ",type(c))
+print("The data type of d is ",type(d))
