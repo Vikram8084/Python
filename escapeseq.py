2543566b93c2159f973bcc59fad64091049a9eed
Vikram Kumar <vikrambr8084@gmail.com>
2023-12-18 21:57:28 +0530

    use of escape seq. character

---
+++ escapeseq.py
@@ -0,0 +1,10 @@
+#is used for single line comment
+print("I am working on VS code")
+'''
+under this we can write multiple line conmment
+Here we are using different parameter of
+print statement
+'''
+print("Hello",22,"Bye",sep="--")
+#using escape character
+print("Hello\n This is VIKRAM\nfrom Indore")
