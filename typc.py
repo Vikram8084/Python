74d32a4ec96d6d54d06b6583dd33169eb2bf47e2
Vikram Kumar <vikrambr8084@gmail.com>
2023-12-19 22:58:26 +0530

    Typecating in python

---
+++ typc.py
@@ -0,0 +1,15 @@
+#eplicit typecasting
+a="10"
+b="5"
+print(type(a))
+c=int(a)+int(b)
+print(c)
+print(type(c))
+#impicit typecasting
+a=7
+print(type(a))
+b=8.0
+print(type(b))
+c=a+b
+print(c)
+print(type(c))
