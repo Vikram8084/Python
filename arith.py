c4010445dd0be2bf14558cc7b8a6156ae2fcc997
Vikram Kumar <vikrambr8084@gmail.com>
2023-12-19 21:40:14 +0530

    arithmatic operation

---
+++ arith.py
@@ -0,0 +1,16 @@
+# here we can do all mathematical operation
+a=10
+b=5
+add=a+b #addition
+print("The sum of two number is",add)
+sub=a-b #subtraction
+print("The subtraction of two number is ",sub)
+mul=a*b #multiplication
+print("The multiplication of two number is ",mul)
+div=a/b #division
+print("The division of two number is ",div)
+mod=a%b #modulus operator
+print("The modulus of two number is ",mod)
+flo=a//b #floor division
+print("The floor division of two number is ",flo)
+
