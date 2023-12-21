6a5dccb8fd48f44e1e8178eaa75e1c6851e7a8e5
Vikram Kumar <vikrambr8084@gmail.com>
2023-12-21 21:19:57 +0530

    diiferent function

---
+++ stringop.py
@@ -0,0 +1,14 @@
+a="Vikram Kumar"
+print(a.upper())#to convert all the character in upper case
+print(a.lower())# to convert all the character in lower case
+print(a.replace("Kumar","Singh")) #used to replace string
+print(a.split("  "))
+print(a.capitalize())
+print(a.count( "Vikram"))#count the no of occurance
+print(a.isspace())
+print(a.isalpha())
+str1=" Welcome to the console of vs code !!!"
+print(str1.endswith("!!!"))
+print(str1.endswith("to",4,16))
+print(str1.find("to"))
+print(str1.isprintable())
