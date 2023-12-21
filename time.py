d52dc6980daee9091b656e9cb4f6f6e0e49960af
Vikram Kumar <vikrambr8084@gmail.com>
2023-12-21 23:11:08 +0530

    to display time

---
+++ time.py
@@ -0,0 +1,9 @@
+import time
+timestamp=time.strftime('%H : %M : %S')
+print(timestamp)
+timestamp=time.strftime('%H')
+print(timestamp,"Hours")
+timestamp=time.strftime('%M')
+print(timestamp,"Minutes")
+timestamp=time.strftime('%S')
+print(timestamp,"Seconds")
