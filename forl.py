e879402bc5edd06c1ad156bf5f5394303c678452
Vikram Kumar <vikrambr8084@gmail.com>
2023-12-22 21:35:59 +0530

    use of for loop

---
+++ forl.py
@@ -0,0 +1,20 @@
+#for loop for string
+a="VIKRAM"
+for i in a:
+    print(i)
+#for list
+lst1=["vikram","chetan","alok","aryan"]
+for name in lst1:
+    print(name)
+    for a in name:
+        print(a)
+#for iterating the number
+for c in range(10):
+    print(c+1)
+# for given range
+print("between the given range")
+for k in range(5,10):
+    print(k)
+print("giving the step in the range")
+for g in range(5,21,2):
+    print(g)
